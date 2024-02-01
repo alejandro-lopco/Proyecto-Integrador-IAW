e123e43ce4a58e8339ddd8827c2f46f2e87ef9d1
alejandro-lopco <alejandro.lopco@gmail.com>
2024-02-01 20:08:17 +0100

    plantilla para la creación de futuras páginas

---
+++ plantilla.js
@@ -0,0 +1,6 @@
+function abrirDeslizable() {
+    document.getElementById("deslizable").style.height = "500px";
+}
+function cerrarDeslizable() {
+    document.getElementById("deslizable").style.height = "0px";
+}
