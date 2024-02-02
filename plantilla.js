b28457e005c5b5cbd871a6ba5acb866fce452c82
alejandro-lopco <alejandro.lopco@gmail.com>
2024-02-02 14:00:09 +0100

    Actualización a la plantilla y adición de comentarios

--- plantilla.js
+++ plantilla.js
@@ -1,6 +1,27 @@
-function abrirDeslizable() {
-    document.getElementById("deslizable").style.height = "500px";
+// Por cada opción creada se debe crear una funcion correspondiente
+// Dicha función tiene que corresponder con las id únicas
+// Si no se hace esto, se cambiara todo los elementos con el mismo ID
+function abrirDeslizable1() {
+    document.getElementById("tituloOpcion1").style.color = "white";
+    document.getElementById("tituloOpcion1").style.transition = "500ms";
+    document.getElementById("deslizableOpcion1").style.height = "200px";
+    document.getElementById("deslizableOpcion1").style.transition = "500ms";
 }
-function cerrarDeslizable() {
-    document.getElementById("deslizable").style.height = "0px";
+function cerrarDeslizable1() {
+    document.getElementById("tituloOpcion1").style.color = "black";
+    document.getElementById("tituloOpcion1").style.transition = "500ms";
+    document.getElementById("deslizableOpcion1").style.height = "0px";
+    document.getElementById("deslizableOpcion1").style.transition = "500ms";
+}
+function abrirDeslizable2() {
+    document.getElementById("tituloOpcion2").style.color = "white";
+    document.getElementById("tituloOpcion2").style.transition = "500ms";
+    document.getElementById("deslizableOpcion2").style.height = "200px";
+    document.getElementById("deslizableOpcion2").style.transition = "500ms";
+}
+function cerrarDeslizable2() {
+    document.getElementById("tituloOpcion2").style.color = "black";
+    document.getElementById("tituloOpcion2").style.transition = "500ms";
+    document.getElementById("deslizableOpcion2").style.height = "0px";
+    document.getElementById("deslizableOpcion2").style.transition = "500ms";
 }
